05f88aa9785a4b93eccc2fc27faf3694d0452f13
GabrielMcryu <gabrielmcryu@gmail.com>
2022-11-07 13:11:12 +0300

    Included models in admin panel

--- afya/healthcare/admin.py
+++ afya/healthcare/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
 # Register your models here.
+from healthcare.models import UserProfile, DoctorProfile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+
+class UserProfileInLine(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+
+class AccountsUserAdmin(AuthUserAdmin):
+    list_display = ('id', 'username', 'email')
+    search_fields = ('id', 'username', 'email')
+    def add_view(self, *args, **kwargs):
+        self.inlines = []
+        return super(AccountsUserAdmin, self).add_view(*args, **kwargs)
+    
+    def change_view(self, *args, **kwargs):
+        self.inlines = [UserProfileInLine]
+        return super(AccountsUserAdmin, self).change_view(*args, **kwargs)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    readonly_fields = ['id', 'user_id']
+    fields = ['id', 'phone', 'gender', 'birth_date', 'user_id']
+    autocomplete_fields = ['user_id']
+    list_display = ['id', 'phone', 'birth_date', 'user_id']
+
+
+
+class DoctorProfileAdmin(admin.ModelAdmin):
+    model = DoctorProfile
+    list_display = ['id', 'doctor_name', 'specialization', 'county', 'biography']
+
+    def doctor_name(self, obj):
+        return obj.user.username
+    
+    def user_id(self, obj):
+        return obj.user.id
+
+admin.site.unregister(User)
+admin.site.register(DoctorProfile, DoctorProfileAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(User, AccountsUserAdmin)
